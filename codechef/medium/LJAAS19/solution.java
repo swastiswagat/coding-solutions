3a952ced4f1ab4cf988dcb741316ca423aa99d67
Swasti Swagat <swagatswasti@gmail.com>
2026-08-07 20:44:29 +0530

    ✅ LJAAS19 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-07

---
+++ codechef/medium/LJAAS19/solution.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Your code here
+        System.out.println("i");
+        System.out.println("love");
+        System.out.println("Codechef");
+    }
+}
