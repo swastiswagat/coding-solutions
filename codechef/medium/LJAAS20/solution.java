5420a201ef25d92fcef55a5000032a7605b4dbd1
Swasti Swagat <swagatswasti@gmail.com>
2026-08-07 20:47:54 +0530

    ✅ LJAAS20 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-07

--- codechef/medium/LJAAS20/solution.java
+++ codechef/medium/LJAAS20/solution.java
@@ -1,8 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
-        // Your code here
-        System.out.println("i");
-        System.out.println("love");
-        System.out.println("Codechef");
-    }
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+	    System.out.println(1 + " - " + (1 * 1));
+        System.out.println(2 + " - " + (2 * 2));
+        System.out.println(3 + " - " + (3 * 3));
+        System.out.println(4 + " - " + (4 * 4));
+        System.out.println(5 + " - " + (5 * 5));
+		
+
+	}
 }
