1b440f996bbaf1fb69aa9402c0e000288bfc00b7
Swasti Swagat <swagatswasti@gmail.com>
2026-08-13 23:48:11 +0530

    ✅ LJAAS29 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ codechef/medium/LJAAS29/solution.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Write your code here 
+        int x = 20;
+        int y = 6;
+        System.out.println(x/y);
+        
+    }
+}
