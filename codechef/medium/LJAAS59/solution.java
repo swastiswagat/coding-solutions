72a0af6a996d4edb3b629d77abb39f054828d03d
Swasti Swagat <swagatswasti@gmail.com>
2026-08-18 20:26:50 +0530

    ✅ LJAAS59 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/medium/LJAAS59/solution.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        num = scanner.nextInt();
+
+        if (num > 0)
+            System.out.println("Positive");
+        else if (num < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+
+        num = scanner.nextInt();
+
+        if (num > 0)
+            System.out.println("Positive");
+        else if (num < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+
+        num = scanner.nextInt();
+
+        if (num > 0)
+            System.out.println("Positive");
+        else if (num < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+}
