25712beab0dfaa3b8740413ded0da7baf5ec69a8
Swasti Swagat <swagatswasti@gmail.com>
2026-08-21 17:45:34 +0530

    ✅ Middle of Three | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ gfg/basic/middle-of-three2926/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    int middle(int a, int b, int c) {
+        if ((a > b && a < c) || (a < b && a > c)) {
+            return a;
+        }
+        else if ((b > a && b < c) || (b < a && b > c)) {
+            return b;
+        }
+        else {
+            return c;
+        }
+    }
+}
