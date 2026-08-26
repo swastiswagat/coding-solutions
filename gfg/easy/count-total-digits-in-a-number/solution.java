a6b2d5a702555205754480eb4bc48dfce00db169
Swasti Swagat <swagatswasti@gmail.com>
2026-08-26 23:10:02 +0530

    ✅ Count Digits in a Number | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ gfg/easy/count-total-digits-in-a-number/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int countDigits(int n) {
+        // Code here
+        int count = 0;
+        while (n > 0){
+        int  digit = n % 10;
+            n = n / 10;
+            count++;}
+        return count;
+    }
+}
