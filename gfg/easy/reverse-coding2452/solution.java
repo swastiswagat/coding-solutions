5a235143d6b81dcc09aa74495be6ab19e0bdff7d
Swasti Swagat <swagatswasti@gmail.com>
2026-08-26 20:05:52 +0530

    ✅ Sum of Natural Numbers | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ gfg/easy/reverse-coding2452/solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        // code here
+        for (int i = 1; i <= n; i++){
+            sum += i;
+        }
+            System.out.println(sum);
+    }
+}
