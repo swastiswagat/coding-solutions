4b222ba713253721f6fa1f2841688a7eb43e0b98
Swasti Swagat <swagatswasti@gmail.com>
2026-08-26 19:52:11 +0530

    ✅ While Loop | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ gfg/basic/while-loop/solution.java
@@ -0,0 +1,10 @@
+// import java.util.Scanenr;
+class Solution {
+    public static void utility(int x) {
+        // code here
+        // Scanner sc = new Scanner(System.in);
+        for(int i = x; i >= 0; i--) {
+            System.out.print(i+" ");
+        }
+    }
+}
