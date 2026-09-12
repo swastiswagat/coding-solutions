a4d1c23bcf5106a85668d779529a178695263244
Swasti Swagat <swagatswasti@gmail.com>
2026-09-12 17:50:38 +0530

    ✅ JPRACMCQ6 - Rating 288 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-12

---
+++ codechef/easy/JPRACMCQ6---Rating-288/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		// write your code here
+		if (y > x){
+		    System.out.println("YES");
+		}else {
+		    System.out.println("NO");
+		}
+	}
+}
