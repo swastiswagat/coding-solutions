d78a530c93c2f25618a913dd6ef968a061cb78c5
Swasti Swagat <swagatswasti@gmail.com>
2026-09-12 18:00:59 +0530

    ✅ ONEFULPAIRS - Rating 374 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-12

---
+++ codechef/easy/ONEFULPAIRS---Rating-374/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		// write your code here
+	    System.out.println((a + b + (a * b) == 111) ? "Yes" : "No");
+		
+	}
+}
